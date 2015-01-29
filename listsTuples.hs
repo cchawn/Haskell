d25450857c38c14a90729d71e62e65f0cc3a7b19
Christina Chan <christinaavchan@gmail.com>
2015-01-29 12:18:37 -0500

    5.1

--- listsTuples.hs
+++ listsTuples.hs
@@ -1,5 +1,21 @@
 module ListsTuples where
--- 5.1
+-- 5.1 Define a function which returns the
+-- max of two integers, togehter with the number
+-- of times it occurs, using this function define
+-- another function which does same for 3 arguments
+maxOccurs :: Int -> Int -> (Int, Int)
+maxOccurs x y
+	| x == y = (x, 2)
+	| x < y = (y, 1)
+	| otherwise = (x, 1)
+
+maxThreeOccurs :: Int -> Int -> Int -> (Int, Int)
+maxThreeOccurs x y z
+	| z == maxTwo = (z, occursTwo + 1)
+	| z > maxTwo = (z, 1)
+	| otherwise = (maxTwo, occursTwo)
+	where (maxTwo, occursTwo) = maxOccurs x y
+
 -- 5.2
 -- 5.16
 -- 5.17
