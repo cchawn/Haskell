d791eb2cecf3a9dea789329e58640354089a4983
Christina Chan <christinaavchan@gmail.com>
2015-01-29 12:36:55 -0500

    5.2

--- listsTuples.hs
+++ listsTuples.hs
@@ -16,13 +16,48 @@ maxThreeOccurs x y z
 	| otherwise = (maxTwo, occursTwo)
 	where (maxTwo, occursTwo) = maxOccurs x y
 
--- 5.2
--- 5.16
+-- 5.2 Define a function that puts the elements of 
+-- a triple into ascending order.
+-- from text (page 39)
+maxThree :: Int -> Int -> Int -> Int
+maxThree x y z
+	| x >= y && x >= z = x
+	| y >= z = y
+	| otherwise = z
+  
+-- from solution to 3.11
+minThree :: Int -> Int -> Int -> Int
+minThree a b c
+	| a <= b && a <= c = a
+	| b <= c = b
+	| otherwise = c
+
+-- from text (page 57)
+middleNumber :: Int -> Int -> Int -> Int
+middleNumber x y z
+	| between y x z = x
+	| between x y z = y
+	| otherwise = z
+
+-- helper function for middleNumber
+between a b c 
+	| a <= c = a <= b && b <= c
+	| otherwise = a >= b && b >= c
+
+orderThree :: (Int, Int, Int) -> (Int, Int, Int)
+orderThree (x, y, z) =
+	((minThree x y z), (middleNumber x y z), (maxThree x y z))
+
+-- 5.16 How many items does the list [2,3] contain?
+-- how many does [[2,3]] contain? What is the type?
+
+
 -- 5.17
 -- 5.18
 -- 5.19
 -- 5.20
 -- 5.21
+
 -- 7.5
 -- 7.6
 -- 7.8
